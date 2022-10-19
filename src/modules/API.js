b37d5c5e9ccfc1f20eb9454a90851c65dcf4a477
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-10-19 16:41:25 +0530

    Add updated js file

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,48 +1,46 @@
 class Score {
-    constructor(user, score) {
-        this.user = user;
-        this.score = score;
-    }
-    scoreData = [];
-    url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/:id';
+  constructor(user, score) {
+    this.user = user;
+    this.score = score;
+  }
 
-    getScore = () => {
-        const recentScore = document.querySelector('.recent-score-group');
-        recentScore.innerHTML = this.scoreData.map((element, index) => `<li class=${index % 2 !== 0 ? 'list-white' : 'list-gray'}>${element.user} : ${element.score}</li>`).join('');
-    }
+  scoreData = [];
 
-    fetchScore = async () => {
-        try {
-            const data = await fetch(this.baseUrl);
-            const res = await data.json();
-            const displayData = res.result.map((item) => item);
-            return this.getScore();
+  url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/:id';
+
+  getScore = () => {
+    const recentScore = document.querySelector('.recent-score-group');
+    recentScore.innerHTML = this.scoreData.map((element, index) => `<li class=${index % 2 !== 0 ? 'list-white' : 'list-gray'}>${element.user} : ${element.score}</li>`).join('');
+  }
+
+  fetchScore = async () => {
+    try {
+        const data = await fetch(this.baseUrl);
+        const res = await data.json();
+        const displayData = res.result.map((item) => item);
+        return this.getScore();
         }
-        catch (err) {
-            return err;
-        }
-    };
+    catch (err) { return err; }
+  };
 
-    addScore = async ({ user, scoreNum}) => {
-        try {
-            const newgame = { user, score: scoreNum };
-            const config = {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-type': 'application/json',
+  addScore = async ({ user, scoreNum}) => {
+    try {
+        const newgame = { user, score: scoreNum };
+        const config = {
+        method: 'POST',
+        headers: {
+        Accept: 'application/json',
+                'Content-type': 'application/json',
                 },
                 body: JSON.stringify(newgame),
-            };
-            const data = await fetch(this.url, config);
-            this.scoreData.push(newgame);
-            this.getScore();
-            return data;
+        };
+        const data = await fetch(this.url, config);
+        this.scoreData.push(newgame);
+        this.getScore();
+        return data;
         }
-        catch (err) {
-            return err;
-        }
-    }
+        catch (err) { return err; }
+  }
 }
 
 export default Score;
